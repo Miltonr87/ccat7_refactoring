84a54ebb3ff5e476534d7fb171cba26b3c7b25b9
Milton Rodrigues <mneto@close-upinternational.com.br>
2022-07-01 22:33:44 -0300

    Testing Code Smell

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -1,6 +1,7 @@
 import { calc } from '../src/calc';
 
 test("Deve calcular o valor da corrida normal", () => {
-    const fare = calc([{ dist: 10, ds: new Date("2021-03-01t10:00:00") }])
+    const codeSmell = [{ dist: 10, ds: new Date("2021-03-01t10:00:00") }];
+    const fare = calc(codeSmell);
     expect(fare).toBe(10);
 })
