77efbe9e923c12e19022513740eb628dc34b4dcc
Milton Rodrigues <mneto@close-upinternational.com.br>
2022-07-01 23:34:34 -0300

    Update calc.test.ts

--- test/calc.test.ts
+++ test/calc.test.ts
@@ -3,5 +3,7 @@ import { calc } from '../src/calc';
 test("Deve calcular o valor da corrida normal", () => {
     const codeSmell = [{ dist: 10, ds: new Date("2021-03-01t10:00:00") }];
     const fare = calc(codeSmell);
-    expect(fare).toBe(10);
+    expect(fare).toBe(21);
+    // pode retornar .toThrow("Erro Fulano de Tal") para verificar se ocorreu algum erro e não devolver um valor qualquer
 })
+
