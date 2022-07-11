e17b4689f5ec1d10f9809b01d42687d69e6707cf
Milton Rodrigues <mneto@close-upinternational.com.br>
2022-07-11 19:39:50 -0300

    Added CPF with refactoring and tests

--- src/cpf.ts
+++ src/cpf.ts
@@ -1,61 +1,54 @@
-// @ts-nocheck
-export function validate (str) {
+export default class Cpf {
+    private value: string;
+    DIGIT_1_FACTOR = 10;
+    DIGIT_2_FACTOR = 11;
 
-	if (str !== null) {
-        if (str !== undefined) {
-            if (str.length >= 11 || str.length <= 14){
+    constructor(value: string) {
+        if (!this.validate(value)) throw new Error("Cpf Inválido");
+        this.value = value;
+    }
 
-                str=str
-                    .replace('.','')
-                    .replace('.','')
-                    .replace('-','')
-                    .replace(" ","");  
-    
-                if (!str.split("").every(c => c === str[0])) {
-                    try{  
-                        let     d1, d2;  
-                        let     dg1, dg2, rest;  
-                        let     digito;  
-                            let     nDigResult;  
-                        d1 = d2 = 0;  
-                        dg1 = dg2 = rest = 0;  
-                            
-                        for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                            // 	return false;
-                            // } else {
-    
-                                digito = parseInt(str.substring(nCount -1, nCount));  							
-                                d1 = d1 + ( 11 - nCount ) * digito;  
-                
-                                d2 = d2 + ( 12 - nCount ) * digito;  
-                            // }
-                        };  
-                            
-                        rest = (d1 % 11);  
-                
-                        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-                        d2 += 2 * dg1;  
-                        rest = (d2 % 11);  
-                        if (rest < 2)  
-                            dg2 = 0;  
-                        else  
-                            dg2 = 11 - rest;  
-                
-                            let nDigVerific = str.substring(str.length-2, str.length);  
-                        nDigResult = "" + dg1 + "" + dg2;  
-                        return nDigVerific == nDigResult;
-                    }catch (e){  
-                        console.error("Erro !"+e);  
-    
-                        return false;  
-                    }  
-                } else return false
-    
-            }else return false;
+    validate(cpf: string) {
+        if (!cpf) return false;
+        cpf = this.removeNonDigits(cpf);
+        if (!this.isValidLength(cpf)) return false;
+        if (this.allDigitsTheSame(cpf)) return false;
+        const digit1 = this.calculateDigit(cpf, this.DIGIT_1_FACTOR);
+        const digit2 = this.calculateDigit(cpf, this.DIGIT_2_FACTOR);
+        let checkDigit = this.extractCheckDigit(cpf);
+        const calculatedCheckDigit = `${digit1}${digit2}`;
+        return checkDigit == calculatedCheckDigit;
+    }
+
+    removeNonDigits(cpf: string) {
+        return cpf.replace(/\D+/g, "");
+    }
+
+    isValidLength(cpf: string) {
+        return cpf.length === 11;
+    }
+
+    allDigitsTheSame(cpf: string) {
+        const [firstDigit] = cpf;
+        return [...cpf].every(digit => digit === firstDigit);
+    }
+
+    calculateDigit(cpf: string, factor: number) {
+        let total = 0;
+        for (const digit of cpf) {
+            if (factor > 1) {
+                total += parseInt(digit) * factor--;
+            }
         }
+        const rest = total % 11;
+        return (rest < 2) ? 0 : 11 - rest;
+    }
 
+    extractCheckDigit(cpf: string) {
+        return cpf.slice(-2);
+    }
 
-	} else return false;
-
+    getValue() {
+        return this.value;
+    }
 }
